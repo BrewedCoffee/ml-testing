ca689f9de9ef8d95f27d4df46b6d8a7d2e6ee797
Eugene Hong <eugeneh1217@gmail.com>
2021-04-25 15:08:14 -0400

    algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -2,30 +2,51 @@ import scipy.stats as stats
 import matplotlib.pyplot as pyplot
 import numpy
 import pandas
+import sklearn.linear_model as linear_model
+import sklearn.metrics as metrics
 
 from data import Data
 
-data = Data()
+class Algorithm:
+    data = Data()
 
-class LinearRegression:
-    def fit(df):
-        x = df['Sepal Length'].to_list()
-        y = df['Sepal Width'].to_list()
+    @classmethod
+    def on_iris(cls):
+        raise NotImplementedError
 
+class Classifier(Algorithm):
+    @classmethod
+    def train(cls):
+        raise NotImplementedError
+
+    @classmethod
+    def classify(cls):
+        raise NotImplementedError
+
+class LinearRegression(Algorithm):
+    @classmethod
+    def on_iris(cls):
+        return cls.fit(
+            cls.data.iris['Sepal Length'].to_list(),
+            cls.data.iris['Sepal Width'].to_list())
+
+    @staticmethod
+    def fit(x, y):
         result = stats.linregress(x, y)
-
         print(f"R-squared: {result.rvalue**2:.6f}")
         pyplot.plot(x, y, 'o', label='original data')
-
         for i in range(len(y)):
             y[i] = result.intercept + result.slope*x[i]
-
         pyplot.plot(x, y, 'r', label='fitted line')
         pyplot.show()
 
-class LogisticRegression:
+class LogisticRegression(Classifier):
+    @staticmethod
     def train(data):
-        pass
+        print(data)
 
+    @staticmethod
     def classify(model, data):
         pass
+
+LinearRegression.on_iris()
