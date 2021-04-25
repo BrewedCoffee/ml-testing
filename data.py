7c27e875e530e0d672448109d1721f15ece73524
Eugene Hong <eugeneh1217@gmail.com>
2021-04-25 12:51:24 -0400

    initialized data/ and get_iris()

--- data.py
+++ data.py
@@ -1,8 +1,16 @@
 import requests
+import os
 
 IRIS_URL = ('https://archive.ics.uci.edu/ml/'
             'machine-learning-databases/'
             'iris/iris.data')
+DATA_PATH = 'data/'
 
-def get_data():
+def get_iris():
+    IRIS_PATH = DATA_PATH + 'iris.csv'
+    if os.path.isfile(IRIS_PATH):
+        return
     response = requests.get(IRIS_URL)
+    os.makedirs(DATA_PATH)
+    with open(IRIS_PATH, 'wb') as f:
+        f.write(response.content)
