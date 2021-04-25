22edd12d33284a19bdd2ce6aec18368288ae7561
Eugene Hong <eugeneh1217@gmail.com>
2021-04-25 14:01:52 -0400

    refactored data.py to Data.py

---
+++ Data.py
@@ -0,0 +1,23 @@
+import requests
+import os
+import io
+
+import pandas as pd
+
+DATA_PATH = 'data/'
+
+IRIS_URL = ('https://archive.ics.uci.edu/ml/'
+            'machine-learning-databases/'
+            'iris/iris.data')
+IRIS_NAMES = ['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width', 'Class']
+
+class Data:
+    def __init__(self):
+        get_iris()
+
+    def get_iris():
+        csv_data = io.StringIO(requests.get(IRIS_URL).content.decode())
+        df = pd.read_csv(csv_data, names=IRIS_NAMES)
+        return df
+
+print(Data.get_iris())
