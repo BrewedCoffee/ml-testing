c3086f2fec5232ec86ba9eadf21ee1a4e45c8b17
Jeff <guywholikessoda@gmail.com>
2021-04-25 14:11:01 -0400

    6

--- algos/linear_regression.py
+++ algos/linear_regression.py
@@ -1,6 +1,23 @@
-import scipy.stats 
+import scipy.stats as sp
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+from data import Data
 
 def process_data(df):
-    return None
+    x = df['Sepal Length'].to_list()
+    y = df['Sepal Width'].to_list()
+
+    result = sp.linregress(x, y)
+
+    print(f"R-squared: {result.rvalue**2:.6f}")
+    plt.plot(x, y, 'o', label='original data')
+
+    for i in range(len(y)):
+        y[i] = result.intercept + result.slope*x[i]
+
+    plt.plot(x, y, 'r', label='fitted line')
+    plt.show()
+
+process_data(Data.iris)
--- data.py
+++ data.py
@@ -13,7 +13,7 @@ IRIS_NAMES = ['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width', 'Cla
 
 class Data:
     def __init__(self):
-        self.data = get_iris()
+        self.iris = get_iris()
 
     def get_iris():
         csv_data = io.StringIO(requests.get(IRIS_URL).content.decode())
