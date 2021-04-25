053cf7cb2354c2f1185ed754544dfa47094cf525
Jeff <guywholikessoda@gmail.com>
2021-04-25 12:44:20 -0400

    read

--- data.py
+++ data.py
@@ -1,8 +1,16 @@
 import requests
 
+import pandas as pd
+
 IRIS_URL = ('https://archive.ics.uci.edu/ml/'
             'machine-learning-databases/'
             'iris/iris.data')
 
 def get_data():
     response = requests.get(IRIS_URL)
+
+def read_data():
+    df = pd.read_csv('iris.csv')
+    print(df.to_dict())
+
+read_data()
